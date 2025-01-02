50a13ca132b5d02521a5d5b7974dba8016e20b63
Abdulmalek Almkainzi <aalmkainzi@gmail.com>
2025-01-03 02:09:59 +0300

    got rid of compiler warnign for discarded value

--- callater.h
+++ callater.h
@@ -2,7 +2,7 @@
 #define CALLATER_H
 
 #define Invoke(func, timeOrArg, ...) \
-CallaterInvokeLater(func, (NULL __VA_OPT__(, timeOrArg)), (timeOrArg __VA_OPT__(, __VA_ARGS__)))
+CallaterInvokeLater(func, (__VA_OPT__((void)) NULL __VA_OPT__(, timeOrArg)), (__VA_OPT__((void)) timeOrArg __VA_OPT__(, __VA_ARGS__)))
 
 void CallaterInvokeLater(void(*func)(void*), void* arg, float delay);
 void CallaterUpdate();
