8eaadf3f5a8412e9883828120f7d3db1d00661cc
Abdulmalek Almkainzi <aalmkainzi@gmail.com>
2025-01-02 17:11:57 +0300

    changes

--- callater.c
+++ callater.c
@@ -5,57 +5,63 @@
 #include <stdint.h>
 #include <immintrin.h>
 
+#ifdef _WIN32
+#include <windows.h>
+#include <sysinfoapi.h>
+#endif
+
 typedef struct FuncsToCall
 {
     size_t cap;
     size_t count;
-    typeof(void(void*)) **funcs;
-    void **args;
-    float *times;
+    void(**funcs)(void*);
+    void** args;
+    float* times;
 } FuncsToCall;
 
 FuncsToCall table;
 float lastUpdated;
 
-float GetCurrentTime()
+float CallaterCurrentTime()
 {
+#ifdef _WIN32
+    return GetTickCount64() / 1000.0f;
+#else
     struct timespec ts;
-    #ifdef __MINGW32__
-    clock_gettime(CLOCK_REALTIME, &ts);
-    #else
-    timespec_get(&ts, 1);
-    #endif
-    return ts.tv_sec + (ts.tv_nsec / 1000000000.0f);
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+
+    return ts.tv_sec + ts.tv_nsec / 1000000000.0f;
+#endif
 }
 
 void CallaterInit()
 {
-    lastUpdated = GetCurrentTime();
+    lastUpdated = CallaterCurrentTime();
 
     table.count = 0;
     table.cap = 64;
     table.funcs = calloc(table.cap, sizeof(*table.funcs));
-    table.args  = calloc(table.cap, sizeof(*table.args));
+    table.args = calloc(table.cap, sizeof(*table.args));
     table.times = calloc(table.cap, sizeof(*table.times));
 }
 
 static void MaybeGrowTable()
 {
-    if(table.cap <= table.count)
+    if (table.cap <= table.count)
     {
         table.cap *= 2;
-        table.funcs = realloc(table.funcs,    table.cap * sizeof(*table.funcs));
-        table.args  = realloc(table.args,     table.cap * sizeof(*table.args));
-        table.times = realloc(table.times,    table.cap * sizeof(*table.times));
+        table.funcs = realloc(table.funcs, table.cap * sizeof(*table.funcs));
+        table.args = realloc(table.args, table.cap * sizeof(*table.args));
+        table.times = realloc(table.times, table.cap * sizeof(*table.times));
     }
 }
 
-static void InsertFunc(void(*func)(void*), void *arg, float delay)
+static void InsertFunc(void(*func)(void*), void* arg, float delay)
 {
     MaybeGrowTable();
     table.funcs[table.count] = func;
     table.times[table.count] = delay;
-    table.args[table.count]  = arg;
+    table.args[table.count] = arg;
     table.count += 1;
 }
 
@@ -65,53 +71,53 @@ static void CallAndPop(size_t idx)
 
     table.funcs[idx] = table.funcs[table.count - 1];
     table.times[idx] = table.times[table.count - 1];
-    table.args[idx]  = table.args[table.count - 1];
+    table.args[idx] = table.args[table.count - 1];
 
     table.count -= 1;
 }
 
 void CallaterUpdate()
 {
-    float currentTime = GetCurrentTime();
+    float currentTime = CallaterCurrentTime();
     float deltaTime = currentTime - lastUpdated;
     lastUpdated = currentTime;
-    
-    for(size_t i = 0 ; i < table.count ; i++)
+
+    for (size_t i = 0; i < table.count; i++)
     {
         table.times[i] -= deltaTime;
     }
-    
+
     size_t per8 = table.count / 8;
     size_t remaining = table.count % 8;
-    
-    __m256 zero = {0};
+
+    __m256 zero = { 0 };
     size_t i;
-    for(i = 0 ; i < per8 ; i++)
+    for (i = 0; i < per8; i++)
     {
         __m256 floats;
         memcpy(&floats, table.times + (i * 16), sizeof(__m256));
-        __m256 results = _mm256_cmp_ps (zero, zero, _CMP_LE_OS);
-        int32_t *asInts = (int32_t*)&results;
-        
-        for(int j = 0 ; j < 8 ; j++)
+        __m256 results = _mm256_cmp_ps(zero, zero, _CMP_LE_OS);
+        int32_t* asInts = (int32_t*)&results;
+
+        for (int j = 0; j < 8; j++)
         {
-            if(asInts[j])
+            if (asInts[j])
             {
                 CallAndPop(i * 8 + j);
             }
         }
     }
-    
-    for(size_t j = i ; j < table.count ; j++)
+
+    for (size_t j = i; j < table.count; j++)
     {
-        if(table.times[j] <= 0)
+        if (table.times[j] <= 0)
         {
             CallAndPop(j);
         }
     }
 }
 
-void InvokeLaterArg(void(*func)(void*), void *arg, float delay)
+void InvokeLaterArg(void(*func)(void*), void* arg, float delay)
 {
     InsertFunc(func, arg, delay);
 }
