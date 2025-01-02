a7b5570418be06cd832239381419b1fbafe6dcaf
Abdulmalek Almkainzi <aalmkainzi@gmail.com>
2025-01-03 02:04:36 +0300

    refactor

--- callater.c
+++ callater.c
@@ -10,86 +10,102 @@
 #include <sysinfoapi.h>
 #endif
 
-typedef struct FuncsToCall
+typedef struct CallaterTable
 {
     size_t cap;
     size_t count;
     void(**funcs)(void*);
     void** args;
-    double* times;
-} FuncsToCall;
+    float* times;
+    float lastUpdated;
+} CallaterTable;
 
-FuncsToCall table;
-double lastUpdated;
+static CallaterTable table;
 
-double CallaterCurrentTime()
+float CallaterCurrentTime()
 {
-#ifdef _WIN32
-    return GetTickCount64() / 1000.0;
-#else
+    #ifdef _WIN32
+    return GetTickCount64() / 1000.0f;
+    #else
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC, &ts);
-
-    return ts.tv_sec + ts.tv_nsec / 1000000000.0;
-#endif
+    
+    return ts.tv_sec + ts.tv_nsec / 1000000000.0f;
+    #endif
 }
 
 void CallaterInit()
 {
-    lastUpdated = CallaterCurrentTime();
-
+    table.lastUpdated = CallaterCurrentTime();
+    
     table.count = 0;
     table.cap = 64;
     table.funcs = calloc(table.cap, sizeof(*table.funcs));
-    table.args = calloc(table.cap, sizeof(*table.args));
+    table.args  = calloc(table.cap, sizeof(*table.args));
     table.times = calloc(table.cap, sizeof(*table.times));
 }
 
-static void MaybeGrowTable()
+static void CallaterNoop(void* arg)
+{
+    (void)arg;
+}
+
+static void CallaterPopFunc(size_t idx)
+{
+    table.funcs[idx] = table.funcs[table.count - 1];
+    table.times[idx] = table.times[table.count - 1];
+    table.args[idx] = table.args[table.count - 1];
+    
+    table.count -= 1;
+}
+
+static void CallaterCleanupTable()
+{
+    for (size_t i = 0; i < table.count; i++)
+    {
+        if (table.funcs[i] == CallaterNoop)
+            CallaterPopFunc(i);
+    }
+}
+
+static void CallaterMaybeGrowTable()
 {
     if (table.cap <= table.count)
     {
         table.cap *= 2;
         table.funcs = realloc(table.funcs, table.cap * sizeof(*table.funcs));
-        table.args = realloc(table.args, table.cap * sizeof(*table.args));
+        table.args  = realloc(table.args,  table.cap * sizeof(*table.args));
         table.times = realloc(table.times, table.cap * sizeof(*table.times));
     }
 }
 
-static void InsertFunc(void(*func)(void*), void* arg, double delay)
+static void CallaterInsertFunc(void(*func)(void*), void* arg, float delay)
 {
-    MaybeGrowTable();
+    CallaterMaybeGrowTable();
     table.funcs[table.count] = func;
     table.times[table.count] = delay;
     table.args[table.count] = arg;
     table.count += 1;
+    
+    if (table.count >= 1024)
+        CallaterCleanupTable();
 }
 
-static void CallAndPop(size_t idx)
+static void CallaterCallFunc(size_t idx)
 {
     table.funcs[idx](table.args[idx]);
-
-    table.funcs[idx] = table.funcs[table.count - 1];
-    table.times[idx] = table.times[table.count - 1];
-    table.args[idx] = table.args[table.count - 1];
-
-    table.count -= 1;
+    table.funcs[idx] = CallaterNoop;
 }
 
 void CallaterUpdate()
 {
-    double currentTime = CallaterCurrentTime();
-    double deltaTime = currentTime - lastUpdated;
-    lastUpdated = currentTime;
-
-    for (size_t i = 0; i < table.count; i++)
-    {
-        table.times[i] -= deltaTime;
-    }
-
+    float currentTime = CallaterCurrentTime();
+    float deltaTime = currentTime - table.lastUpdated;
+    table.lastUpdated = currentTime;
+    
     size_t per8 = table.count / 8;
     size_t remaining = table.count % 8;
-
+    
     __m256 zero = { 0 };
     __m256 deltaVec = {
         deltaTime, deltaTime, deltaTime, deltaTime, deltaTime, deltaTime, deltaTime, deltaTime
@@ -99,34 +115,29 @@ void CallaterUpdate()
     for (i = 0; i < per8; i++)
     {
         const size_t curVecIdx = i * 8;
-        *(__m256*)(table.times + curVecIdx) = _mm256_sub_ps (*(__m256*)(table.times + curVecIdx), deltaVec);
-        
-        __m256 results = _mm256_cmp_ps  (*(__m256*)(table.times + curVecIdx), zero, _CMP_LE_OS);
-        
+        *(__m256*)(table.times + curVecIdx) = _mm256_sub_ps(*(__m256*)(table.times + curVecIdx), deltaVec);
+        __m256 results = _mm256_cmp_ps(*(__m256*)(table.times + curVecIdx), zero, _CMP_LE_OS);
+        const int(*asInts)[8] = (void*)&results;
         for (int j = 0; j < 8; j++)
         {
-            if (results[j])
+            if ((*asInts)[j])
             {
-                CallAndPop(curVecIdx + j);
+                CallaterCallFunc(curVecIdx + j);
             }
         }
     }
-
-    for (size_t j = i; j < table.count; j++)
+    
+    for (size_t j = i * 8; j < table.count; j++)
     {
+        table.times[j] -= deltaTime;
         if (table.times[j] <= 0)
         {
-            CallAndPop(j);
+            CallaterCallFunc(j);
         }
     }
 }
 
-void InvokeLaterArg(void(*func)(void*), void* arg, double delay)
+void CallaterInvokeLater(void(*func)(void*), void* arg, float delay)
 {
-    InsertFunc(func, arg, delay);
-}
-
-void InvokeLater(void(*func)(), double delay)
-{
-    InsertFunc(func, NULL, delay);
+    CallaterInsertFunc(func, arg, delay);
 }
--- callater.h
+++ callater.h
@@ -1,23 +1,10 @@
 #ifndef CALLATER_H
 #define CALLATER_H
 
-#define Cat(a, b) a##b
-
-#define IfNoVargs(then, ...) \
-Cat(IfNoVargs, 0 ## __VA_OPT__(1))(then)
-
-#define IfNoVargs0(then) then
-
-#define IfNoVargs01(then) 
-
 #define Invoke(func, timeOrArg, ...) \
-__VA_OPT__( \
-    InvokeLaterArg(func, timeOrArg, (__VA_ARGS__)) \
-) \
-IfNoVargs(InvokeLater(func, timeOrArg), __VA_ARGS__)
+CallaterInvokeLater(func, (NULL __VA_OPT__(, timeOrArg)), (timeOrArg __VA_OPT__(, __VA_ARGS__)))
 
-void InvokeLaterArg(void(*func)(void*), void *arg, double delay);
-void InvokeLater(void(*func)(), double delay);
+void CallaterInvokeLater(void(*func)(void*), void* arg, float delay);
 void CallaterUpdate();
 void CallaterInit();
 
