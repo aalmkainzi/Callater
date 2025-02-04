2d3483e4dcdf4eabae8a90fd87a7d4e904facb28
Abdulmalek Almkainzi <aalmkainzi@gmail.com>
2025-02-04 18:30:29 +0300

    organization

--- samples/sample2/build.bat
+++ samples/sample2/build.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal
 
-gcc -mavx ..\..\callater.c src\main.c src\gameobjects\enemy.c src\gameobjects\enemy_spawner.c src\gameobjects\bullet.c src\gameobjects\player.c src\game.c .\raylib-5.5_win64_mingw-w64\lib\libraylib.a -I "./raylib-5.5_win64_mingw-w64/include" -I ".\include" -I "../.." -lgdi32 -lwinmm -o sample2.exe -ggdb -std=gnu11 -Wall -Wextra
+gcc -mavx ..\..\callater.c src\main.c src\gameobjects\enemy.c src\gameobjects\enemy_spawner.c src\gameobjects\bullet.c src\gameobjects\player.c src\game.c .\external\raylib-5.5_win64_mingw-w64\lib\libraylib.a -I "./external/raylib-5.5_win64_mingw-w64/include" -I ".\include" -I "../.." -lgdi32 -lwinmm -o sample2.exe -ggdb -std=gnu11 -Wall -Wextra
 
 exit /b %errorlevel%
